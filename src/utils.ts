05a7c1cdc53a5692f6c45c56ed30f41dfe8fbc33
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-07 20:30:17 +0100

    utils: Add type annotations

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,14 @@ const WSP = /[\t\n\f ]/;
 const ALPHA = /[A-Za-z]/;
 const CRLF = /\r\n?/g;
 
-export function isSpace(char) {
+export function isSpace(char: string): boolean {
   return WSP.test(char);
 }
 
-export function isAlpha(char) {
+export function isAlpha(char: string): boolean {
   return ALPHA.test(char);
 }
 
-export function preprocessInput(input) {
+export function preprocessInput(input: string): string {
   return input.replace(CRLF, "\n");
 }
