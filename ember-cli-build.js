10fc88af3320826d7280e95655d76fb5d66ffecb
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-07 19:16:18 +0100

    build: Integrate JSHint linting into "ember test"

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,16 +1,18 @@
 var Funnel = require('broccoli-funnel');
 var Rollup = require('broccoli-rollup');
+var JSHint = require('broccoli-jshint');
 var MergeTrees = require('broccoli-merge-trees');
+var concat = require('broccoli-concat');
 
 module.exports = function(/* defaults */) {
   var lib = new Funnel('lib/simple-html-tokenizer', {
     include: ['**/*.js'],
-    destDir: '/'
+    destDir: '/lib/simple-html-tokenizer'
   });
 
   var bundled = new Rollup(lib, {
     rollup: {
-      entry: 'index.js',
+      entry: 'lib/simple-html-tokenizer/index.js',
 
       sourceMap: true,
       dest: 'simple-html-tokenizer.js',
@@ -21,7 +23,15 @@ module.exports = function(/* defaults */) {
 
   var tests = new Funnel('test', {
     files: ['tokenizer-tests.js'],
-    destDir: '/tests'
+    destDir: '/test'
+  });
+
+  var libJSHint = new JSHint(lib);
+  var testsJSHint = new JSHint(tests);
+
+  var allTests = concat(new MergeTrees([tests, libJSHint, testsJSHint]), {
+    outputFile: '/tests/tests.js',
+    inputFiles: ['**/*.js'],
   });
 
   var testSupport = new Funnel('test', {
@@ -35,5 +45,5 @@ module.exports = function(/* defaults */) {
     destDir: '/tests'
   });
 
-  return new MergeTrees([bundled, tests, testSupport, qunit]);
+  return new MergeTrees([bundled, allTests, testSupport, qunit]);
 };
