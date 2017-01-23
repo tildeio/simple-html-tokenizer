0f879abd71bb0cc76005b44955262353ce022abf
Ricardo Mendes <rokusu@gmail.com>
2017-01-23 00:18:33 +0000

    add missing types

--- src/utils.ts
+++ src/utils.ts
@@ -2,14 +2,14 @@ const WSP = /[\t\n\f ]/;
 const ALPHA = /[A-Za-z]/;
 const CRLF = /\r\n?/g;
 
-export function isSpace(char): boolean {
+export function isSpace(char: string): boolean {
   return WSP.test(char);
 }
 
-export function isAlpha(char): boolean {
+export function isAlpha(char: string): boolean {
   return ALPHA.test(char);
 }
 
-export function preprocessInput(input): string {
+export function preprocessInput(input: string): string {
   return input.replace(CRLF, "\n");
 }
