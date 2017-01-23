d916288a446b8bba1c9ac46270149a7db7830258
Ricardo Mendes <rokusu@gmail.com>
2017-01-23 09:14:52 +0000

    improve types

--- src/entity-parser.ts
+++ src/entity-parser.ts
@@ -18,10 +18,12 @@ export default class EntityParser {
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 16));
     }
+
     matches = entity.match(CHARCODE);
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 10));
     }
+
     matches = entity.match(NAMED);
     if (matches) {
       return this.named[matches[1]];
--- src/tokenize.ts
+++ src/tokenize.ts
@@ -1,8 +1,8 @@
-import Tokenizer, { Token } from './tokenizer';
+import Tokenizer, { TokenizerOptions } from './tokenizer';
 import EntityParser from './entity-parser';
 import namedCharRefs from './html5-named-char-refs';
 
-export default function tokenize(input, options): Token[] {
+export default function tokenize(input: string, options?: TokenizerOptions) {
   let tokenizer = new Tokenizer(new EntityParser(namedCharRefs), options);
   return tokenizer.tokenize(input);
 }
