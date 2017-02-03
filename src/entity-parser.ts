1cfe9310ab0aa7dbeaef0093a9b03a9ae0aed1b5
Ricardo Mendes <rokusu@gmail.com>
2017-02-03 15:51:17 +0000

    Make entity-parser closer to master branch

--- src/entity-parser.ts
+++ src/entity-parser.ts
@@ -18,12 +18,10 @@ export default class EntityParser {
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 16));
     }
-
     matches = entity.match(CHARCODE);
     if (matches) {
       return String.fromCharCode(parseInt(matches[1], 10));
     }
-
     matches = entity.match(NAMED);
     if (matches) {
       return this.named[matches[1]];
