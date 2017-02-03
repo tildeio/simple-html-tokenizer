d11ff53ae9a08570610d5baca38d191a6f869e69
Ricardo Mendes <rokusu@gmail.com>
2017-02-03 16:00:53 +0000

    add missing type

--- src/tokenize.ts
+++ src/tokenize.ts
@@ -1,8 +1,8 @@
-import Tokenizer, { TokenizerOptions } from './tokenizer';
+import Tokenizer, { Token, TokenizerOptions } from './tokenizer';
 import EntityParser from './entity-parser';
 import namedCharRefs from './html5-named-char-refs';
 
-export default function tokenize(input: string, options?: TokenizerOptions) {
+export default function tokenize(input: string, options?: TokenizerOptions): Token[] {
   let tokenizer = new Tokenizer(new EntityParser(namedCharRefs), options);
   return tokenizer.tokenize(input);
 }
