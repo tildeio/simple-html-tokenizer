a1a299d75a0aff87ea0b82cd7c1e34110d95fb39
Ricardo Mendes <rokusu@gmail.com>
2017-09-20 23:45:46 +0100

    improve types for entity parser

--- src/entity-parser.ts
+++ src/entity-parser.ts
@@ -1,12 +1,16 @@
+interface Dict<T> {
+  [index: string]: T;
+}
+
 const HEXCHARCODE = /^#[xX]([A-Fa-f0-9]+)$/;
 const CHARCODE    = /^#([0-9]+)$/;
 const NAMED       = /^([A-Za-z0-9]+)$/;
 
 export default class EntityParser {
-  constructor(private named) {
+  constructor(private named: Dict<string>) {
   }
 
-  parse(entity) {
+  parse(entity: string): string | undefined {
     if (!entity) {
       return;
     }
